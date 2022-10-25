ae772e39fc2cdbcbb939d63a623ae0c224b30433
pt_va <pt_va@bk.ru>
2022-10-25 21:52:45 +0300

    preparing to add top() function

--- list v1.0.cpp
+++ list v1.0.cpp
@@ -25,6 +25,8 @@ public:
     void push_back(int data);
     int pop_front();
     int pop_back();
+
+    int top();
     
     int size();
     int& operator [](int ind);
@@ -106,7 +108,7 @@ int& list::operator[](int ind)
 }
 
 int list::size() {
-    return size;
+    return msize;
 }
 
 int main() {
